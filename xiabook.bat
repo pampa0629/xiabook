5b282ca401af7ae5884a3855879af56a8dfb8b26
pampa0629 <50463253@qq.com>
2018-09-10 15:49:03 +0800

    必要文件

---
+++ xiabook.bat
@@ -0,0 +1,4 @@
+﻿cd C:\Users\zengzhiming\AppData\Local\Programs\Python\Python36\Lib\site-packages\PyInstaller-3.4.dev0+7817d3fac-py3.6.egg\pyinstaller-develop
+python pyinstaller.py  -F  C:\zengzm\编程\downloadBook\xiabook.py
+xcopy C:\zengzm\编程\downloadBook\conf.ini C:\Users\zengzhiming\AppData\Local\Programs\Python\Python36\Lib\site-packages\PyInstaller-3.4.dev0+7817d3fac-py3.6.egg\pyinstaller-develop\xiabook\dist\ /e /Y
+C:\Users\zengzhiming\AppData\Local\Programs\Python\Python36\Lib\site-packages\PyInstaller-3.4.dev0+7817d3fac-py3.6.egg\pyinstaller-develop\xiabook\dist\xiabook.exe 
